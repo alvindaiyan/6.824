e4f5c09c125c800aecd89c7606293ed44774003e
daiyan <alvindaiyan@gmail.com>
2016-06-30 21:56:40 +1200

    part III 2016 version now :bug:

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -15,7 +18,7 @@ func (mr *Master) schedule(phase jobPhase) {
 		nios = len(mr.files)
 	}
 
-	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
+	log.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
 	// All ntasks tasks have to be scheduled on workers, and only once all of
 	// them have been completed successfully should the function return.
@@ -25,4 +28,37 @@ func (mr *Master) schedule(phase jobPhase) {
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
 	fmt.Printf("Schedule: %v phase done\n", phase)
+
+	// input file name: mr.files[task]
+	// task task
+	done := make(chan bool)
+
+	startWorker := func(index int) {
+		for {
+			workerAddr := <-mr.registerChannel
+			jobArgs := DoTaskArgs{mr.jobName, mr.files[index], phase, index, nios}
+			ok := call(workerAddr, "Worker.DoTask", jobArgs, new(struct{}))
+			log.Println("Register size -----> ", len(mr.registerChannel))
+			if ok {
+				done <- true
+				mr.registerChannel <- workerAddr
+				break
+			}
+		}
+
+	}
+
+	// do the mappers
+	for i:= 0; i < ntasks; i++ {
+		go startWorker(i)
+
+	}
+
+	// lock to wait mapper to finish
+	for i:=0; i < ntasks; i++ {
+		 <-done
+	}
+
+	fmt.Printf("Schedule: %v phase done\n", phase)
+
 }
